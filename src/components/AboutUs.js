12bcf46230995538ddd51797473450dc646f79f1
Jagrutee Banda <jagruteejanardan.banda@philips.com>
2020-03-16 13:01:55 +0530

    Added AboutUs css

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,7 @@
 import React, { useState, Component } from "react";
 import "../App.css";
 import { Button, Col, Image, Card, Row, Container } from "react-bootstrap";
+import "../styles/AboutUs.css";
 import Navbar from "./Navbar";
 
 let drArr = [
@@ -42,28 +43,18 @@ const AboutUs = () => {
         We are team of doctors helping the people to get best of dental
         treatment and help you keep smiling!
       </Row>
-      <Row noGutters>
-        <Col md="3" style={{ height: 900 }}>
-          {/* <Card style={{ width: "2rem", backgroundColor: "lightblue" }}>
-            <Image
-              style={{ height: 300 }}
-              variant="top"
-              src={require("./images/drPrashant.jpg")}
-              roundedCircle
-            />
-            <Card.Body>
-              <Card.Title>{"key.name"}</Card.Title>
-              <Card.Text>{"key.degree"}</Card.Text>
-              <Card.Text>{"key.contact"}</Card.Text>
-              <Button variant="primary">More Info</Button>
-            </Card.Body>
-          </Card> */}
-        </Col>
-        {/* {drArr.map(key => (
-          <Col md="3" style={{ height: 900 }}>
-            <Card style={{ width: "18rem", backgroundColor: "lightblue" }}>
+      <Row noGutters className="doctors-showcase">
+        {drArr.map(key => (
+          <Col md="3" className="no-padding no-margin doctor-tab">
+            <Card
+              style={{
+                width: "18rem",
+                backgroundColor: "lightblue",
+                alignItems: "center"
+              }}
+            >
               <Image
-                style={{ height: 300 }}
+                style={{ height: 200, width: 200, margin: 20 }}
                 variant="top"
                 src={key.img}
                 roundedCircle
@@ -76,7 +67,7 @@ const AboutUs = () => {
               </Card.Body>
             </Card>
           </Col>
-        ))} */}
+        ))}
       </Row>
     </Container>
   );
