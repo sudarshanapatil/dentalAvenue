9aebdf2bcd4b33b01f4f5753407dbf01648ac1b3
Sudarshana Patil <sudarshanagopinath.patil@philips.com>
2020-03-16 19:18:53 +0530

    added upload image function and datepicker

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -1,20 +1,28 @@
-import { Button, Table, Row, Col, Image } from 'react-bootstrap'
+import { Button, Table, Row, Col, Image, Container } from 'react-bootstrap'
 import React, { useState } from 'react'
+import '../styles/CurrentUser.css'
+import Navbar from './Navbar'
+
 function CurrentUser () {
   return (
+    <Container fluid>
+    <Navbar />
     <Row className='currentUserContainer'>
       <Col xs={3}>
         <div className='Row'>Sudarshana Patil</div>
-        <div className='Row'>23</div>
+        <div className='Row'>Age : 23</div>
+      </Col>
+      <Col xs={7}>
       </Col>
 
-      <Col xs={9}>
+      <Col xs={2}>
         <Image
-          src={require('./images/logo.png')}
+          src={require('./images/userLogo.jpg')}
           style={{ width: 100, height: 100 }}
         />
       </Col>
     </Row>
+    </Container>
   )
 }
 export default CurrentUser
--- src/components/ListPatientsTreatment.js
+++ src/components/ListPatientsTreatment.js
@@ -1,53 +1,89 @@
 import { Button, Table, Row, Col, Container } from 'react-bootstrap'
-import React, { useState } from 'react'
+import React, { useState, Component } from 'react'
 import AddEditDeleteMenu from './AddEditDeleteMenu'
 import CurrentUser from './CurrentUser'
-function ListTreatment () {
-  let treatments = [
-    {
-      main: 'X-Ray',
-      sub: '',
-      cost: 2000
-    },
-    {
-      main: 'X-Ray',
-      sub: 'j',
-      cost: 2000
-    },
-    {
-      main: 'X-Ray',
-      sub: '',
-      cost: 2000
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { render } from '@testing-library/react'
+
+class ListPaientsTreatment extends Component {
+  constructor () {
+    super()
+    this.state = {
+      startDate: new Date()
     }
-  ]
-  return (
-    <Container fluid>
+  }
+  handleChange = date => {
+    console.log(this.state.startDate, 'date', date)
+    this.setState({ startDate: date })
+  }
+  handleColor = time => {
+    return time.getHours() > 12 ? "text-success" : "text-error";
+  };
+  render () {
+    // console.log(this.state.startDate, 'date')
+    let treatments = [
+      {
+        main: 'X-Ray',
+        sub: '',
+        cost: 2000
+      },
+      {
+        main: 'X-Ray',
+        sub: 'j',
+        cost: 2000
+      },
+      {
+        main: 'X-Ray',
+        sub: '',
+        cost: 2000
+      }
+    ]
+    return (
+      <Container fluid>
         <CurrentUser />
-      <Row>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Treatment Name</th>
-              <th>Total Amount</th>
-              <th>Amount Paid</th>
-              <th>Balance Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {treatments.map(treatment => (
+        <Row>
+          <Table responsive>
+            <thead>
               <tr>
-                <td>{treatment.date}</td>
-                <td>{treatment.name}</td>
-                <td>{treatment.cost}</td>
-                <td>{treatment.paid}</td>
-                <td>{treatment.balance}</td>
+                <th>Date</th>
+                <th>Treatment Name</th>
+                <th>Total Amount</th>
+                <th>Amount Paid</th>
+                <th>Balance Amount</th>
+                <th>Upload X-ray</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Row>
-    </Container>
-  )
+            </thead>
+            <tbody>
+              {treatments.map(treatment => (
+                <tr>
+                  <td>
+                    <DatePicker showTimeSelect
+                      selected={this.state.startDate}
+                      onChange={this.handleChange}
+                      timeClassName={this.handleColor}
+                    />
+                  </td>
+                  <td>{treatment.main}</td>
+                  <td>{treatment.cost}</td>
+                  <td>{treatment.paid}</td>
+                  <td>{treatment.balance}</td>
+                  <td><input type="file" name="file" /></td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Row>
+        <Row>
+          <Button>
+            Save Details
+          </Button>
+          <Button>
+            Generate Invoice
+          </Button>
+        </Row>
+      </Container>
+    )
+  }
 }
-export default ListTreatment
+export default ListPaientsTreatment
