803798210118d6d0cc3b0ea7a10b589c8c67f62d
Jagrutee Banda <jagruteejanardan.banda@philips.com>
2020-03-16 12:47:18 +0530

    aligining cards in process

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -29,19 +29,38 @@ let drArr = [
     contact: "8778454523"
   }
 ];
-function AboutUs() {
+
+const AboutUs = () => {
   return (
-    <Container fluid>
+    <Container fluid className="no-padding no-margin">
       <Navbar />
-      <Row className="aboutTitle">About Us</Row>
-      <hr></hr>
-      <Row className="aboutText">
-        We are team of doctors helping the people to get best of dental
-        treatment and help you kepp smiling!
+      <Row noGutters className="aboutTitle">
+        About Us
       </Row>
-      <Row>
-        {drArr.map(key => (
-          <Col style={{ height: 900 }}>
+      <hr width="80%"></hr>
+      <Row noGutters className="aboutText">
+        We are team of doctors helping the people to get best of dental
+        treatment and help you keep smiling!
+      </Row>
+      <Row noGutters>
+        <Col md="3" style={{ height: 900 }}>
+          {/* <Card style={{ width: "2rem", backgroundColor: "lightblue" }}>
+            <Image
+              style={{ height: 300 }}
+              variant="top"
+              src={require("./images/drPrashant.jpg")}
+              roundedCircle
+            />
+            <Card.Body>
+              <Card.Title>{"key.name"}</Card.Title>
+              <Card.Text>{"key.degree"}</Card.Text>
+              <Card.Text>{"key.contact"}</Card.Text>
+              <Button variant="primary">More Info</Button>
+            </Card.Body>
+          </Card> */}
+        </Col>
+        {/* {drArr.map(key => (
+          <Col md="3" style={{ height: 900 }}>
             <Card style={{ width: "18rem", backgroundColor: "lightblue" }}>
               <Image
                 style={{ height: 300 }}
@@ -57,9 +76,9 @@ function AboutUs() {
               </Card.Body>
             </Card>
           </Col>
-        ))}
+        ))} */}
       </Row>
     </Container>
   );
-}
+};
 export default AboutUs;
